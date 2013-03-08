ce1a5d91b0d324da9d543213809bf9fba9125aea
Chance Zibolski <chance.zibolski@gmail.com>
2013-03-07 20:45:54 -0800

    Bootstraped

--- lugpolls/urls.py
+++ lugpolls/urls.py
@@ -6,9 +6,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    #url(r'^accounts/profile/', 'user.views.profile', name='auth_profile'),
 )
 
 # This serves up staticfiles at STATIC_URL when debug=True

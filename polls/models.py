4fb9c597c9b7ca15c02fa1673c4bf52da4869602
Chance Zibolski <chance.zibolski@gmail.com>
2013-03-10 20:17:27 -0700

    Add publish date to poll templates

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,10 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', default=timezone.now())
 
+    class Meta:
+        get_latest_by = 'pub_date'
+        ordering = ['-pub_date']
+
     def was_published_recently(self):
         """Returns whether or not the poll was published in the last day"""
         now = timezone.now()
